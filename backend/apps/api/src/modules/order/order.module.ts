d124d7d7a6817ff34a73ba5d4f0d0224f0a9f6e7
joewschang <joewschang@gmail.com>
2026-09-25 18:44:12 +0800

    feat(paper): add receipt entry and approval commands

--- backend/apps/api/src/modules/order/order.module.ts
+++ backend/apps/api/src/modules/order/order.module.ts
@@ -5,7 +5,9 @@ import { PackageConfigModule } from '../package-config/package-config.module';
 import { QualificationModule } from '../qualification/qualification.module';
 import { OrganizationModule } from '../organization/organization.module';
 import { RetailReferrerAttributionService } from './retail-referrer-attribution.service';
+import {PaperReceiptService} from './paper-receipt.service';
+import {PaperReceiptController} from './paper-receipt.controller';
 import { AdminRetailReferrerController } from './admin-retail-referrer.controller';
 
-@Module({ imports:[PackageConfigModule,QualificationModule,OrganizationModule], controllers: [OrderController,AdminRetailReferrerController], providers: [OrderService,RetailReferrerAttributionService], exports:[OrderService,RetailReferrerAttributionService] })
+@Module({ imports:[PackageConfigModule,QualificationModule,OrganizationModule], controllers: [OrderController,AdminRetailReferrerController,PaperReceiptController], providers: [OrderService,RetailReferrerAttributionService,PaperReceiptService], exports:[OrderService,RetailReferrerAttributionService] })
 export class OrderModule {}
